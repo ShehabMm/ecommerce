1bb600abf892ce4882022ed02b28ec638e18dd67
shehabMm <shehab.mm@gmail.com>
2024-01-13 01:27:18 +0200

    menu

--- ecommerce/src/components/header/Header3.jsx
+++ ecommerce/src/components/header/Header3.jsx
@@ -1,13 +1,16 @@
-import { Container, Stack } from "@mui/material";
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import {useState} from 'react'
-import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import DehazeIcon from '@mui/icons-material/Dehaze';
+import { Container, Stack, useTheme, IconButton } from "@mui/material";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
+import ElectricBikeOutlinedIcon from '@mui/icons-material/ElectricBikeOutlined';
+import LaptopChromebookOutlinedIcon from '@mui/icons-material/LaptopChromebookOutlined';
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
 const Header3 = () => {
-
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -16,44 +19,78 @@ const Header3 = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const theme = useTheme();
   return (
-    <Container sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-      <Stack  direction="row" alignItems="center" sx={{backgroundColor:"#777", color:"white", width:"200px"}}> 
-<DashboardIcon/>
-<Button 
-sx={{ mr:5     }}
-size="small"
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        Dashboard
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        mt: 5,
+      }}
+    >
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{
+          backgroundColor: theme.palette.text.secondary,
+          color: "white",
+          width: "200px",
         }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+        <DashboardIcon />
+        <Button
+          sx={{
+            mr: 5,
+            paddingLeft: 3,
+            color: "white",
+            gap: 5,
+            alignItems: "center",
+          }}
+          size="small"
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          Dashboard
+          <KeyboardArrowRightOutlinedIcon />
+        </Button>
 
+        <Menu
+      
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem  sx={{ display:"flex", justifyContent:"left",gap:2, width:"200px"}}onClick={handleClose}>
+            <SportsEsportsOutlinedIcon/>Games</MenuItem>
+          <MenuItem  sx={{ display:"flex", justifyContent:"left",gap:2, width:"200px"}} onClick={handleClose}>  <ElectricBikeOutlinedIcon/> Bikes</MenuItem>
+          <MenuItem  sx={{ display:"flex", justifyContent:"left",gap:2, width:"200px"}} onClick={handleClose}>
+            <LaptopChromebookOutlinedIcon/>
+            Electronics</MenuItem>
+          <MenuItem   sx={{ display:"flex", justifyContent:"left",gap:2, width:"200px"}}onClick={handleClose}>
+            <MenuBookOutlinedIcon/>
+            Books</MenuItem>
 
-<KeyboardArrowRightOutlinedIcon/>
-</Stack>
+        </Menu>
+      </Stack>
 
-<Stack>
-  <DehazeIcon/>
-</Stack>
+      <Stack>
+      <IconButton aria-label="menu" >
+        
+  
+        <DehazeIcon />
+        </IconButton>
+      </Stack>
     </Container>
   );
-}
+};
 
 export default Header3;
