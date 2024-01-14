ed5f6078564477067a283ca43cf995db93d7e121
shehabMm <shehab.mm@gmail.com>
2024-01-14 22:02:58 +0200

    list

--- ecommerce/src/components/header/links.jsx
+++ ecommerce/src/components/header/links.jsx
@@ -1,16 +1,72 @@
-import Typography from '@mui/material/Typography'
-import { ExpandMore } from '@mui/icons-material';
-import {  IconButton, Stack } from '@mui/material';
+import Typography from "@mui/material/Typography";
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Stack,
+} from "@mui/material";
 
 const Links = () => {
   return (
-    <Stack direction="row"  sx={{":hover":{cursor:"pointer"}}}>
-      <Typography variant="body1" color="initial" sx={{mr:1}}>Home</Typography>
+    <Stack
+      direction="row"
+      sx={{
+        ":hover .pap": { display: "block", cursor: "pointer" },
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="body1" color="initial" sx={{ mr: 1 }}>
+        Home
+      </Typography>
       <ExpandMore />
+
+      <Box
+        className="pap"
+        sx={{
+          position: "absolute",
+          top: "100%",
+          width: "170px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          display: "none",
+        }}
+      >
+        <Paper className="pap" sx={{ mt: 2 }}>
+          <nav aria-label="secondary mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Dashboard" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                  <ListItemText primary="Products" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Orders" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                  <ListItemText primary="Profile" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Paper>
+      </Box>
     </Stack>
-
-
   );
-}
+};
 
 export default Links;
