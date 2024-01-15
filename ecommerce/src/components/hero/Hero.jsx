eb196ca790cd70382188aa85f1aae70fa6858032
shehabMm <shehab.mm@gmail.com>
2024-01-15 02:55:39 +0200

    hero

--- ecommerce/src/components/hero/Hero.jsx
+++ ecommerce/src/components/hero/Hero.jsx
@@ -2,20 +2,20 @@ import { Box, Container } from "@mui/material";
 
 const Hero = () => {
   return (
-    <Container sx={{mt:2.5, display:"flex", alignItems:"center"}}>
-<Box>Slider</Box>
+    <Container sx={{ mt: 2.5, display: "flex", alignItems: "center" }}>
+      <Box>Slider</Box>
 
-<Box>
-
-<Box>
-  <img src="../assets/banner-17.jpg" alt="" />
-  </Box>  
-<Box><img src="../assets/banner-16.jpg" alt="" /></Box>  
-
-</Box>
-
+      <Box>
+        <Box>
+          <img src="../assets/banner-17.jpg" alt="shoes picture" />
+        </Box>
+        <Box>
+          
+          <img src="../assets/banner-16.jpg" alt="computer picture" />
+        </Box>
+      </Box>
     </Container>
   );
-}
+};
 
 export default Hero;
