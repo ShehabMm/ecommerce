945b68412d1732f5f419e867a3539062f761027c
shehabMm <shehab.mm@gmail.com>
2024-01-29 04:44:52 +0200

    css

--- ecommerce/src/components/hero/Slider.jsx
+++ ecommerce/src/components/hero/Slider.jsx
@@ -9,7 +9,8 @@ import './stylesss.css';
 
 // import required modules
 import { Pagination } from 'swiper/modules';
-import { Container } from '@mui/material';
+import { Box, Button, Container, Stack } from '@mui/material';
+import { Typography } from '@mui/material';
 
 const Slider = () => {
   return (
@@ -25,8 +26,40 @@ const Slider = () => {
         <SwiperSlide>
           
       
-          <img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1706334825/banner-15_wz3hzx.jpg" alt="" />
+          <img style={{position:"relative"}} src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1706334825/banner-15_wz3hzx.jpg" alt="" />
           
+          <Box sx={{position:"absolute", textAlign:"left", left:100, lineHeight:"20px"}}>
+            <Typography variant="h6" color="initial">LIFE STYLE COLLECTION</Typography>
+
+<Typography variant="h1" color="initial" sx={{fontWeight:"bold"}}>MEN</Typography>
+
+
+<Stack direction="row" sx={{gap:1}}>
+
+<Typography variant="h4" color="initial">SALE UP TO </Typography>
+
+<Typography variant="h4" color="error">30% OFF</Typography>
+
+</Stack>
+<Typography variant="body1" color="initial">Get Free Shipping On orders over 99.00$</Typography>
+ <Button
+ 
+ sx={{
+
+
+px:5, py:1, mt:2, backgroundColor:"#222", boxShadow:"0px 4px 16px rgba(43,52,69,0.1)",
+color:"#fff", borderRadius:"1px", "&:hover":{
+
+bgcolor:"#151515", boxShadow:"0px 4px 16px rgba(43,52,69,0.1)",}
+
+
+
+ }}
+ variant="contained"
+ 
+ >SHOP NOW</Button>
+
+          </Box>
           </SwiperSlide>
 
 
