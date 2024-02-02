f48642a2195c6b1d7e5d935cae7617ae2c893a9a
shehabMm <shehab.mm@gmail.com>
2024-02-02 15:49:23 +0200

    cs

--- ecommerce/src/components/main/scrollup.jsx
+++ ecommerce/src/components/main/scrollup.jsx
@@ -27,7 +27,7 @@ if (window.scrollY>300) {
        
 
   <a href="#up"  style={{opacity:hidden?1:0, transition:"all 0.3s"}}> 
-  <Box  sx={{"&:active":{bgcolor:"blue"},  "&:hover":{cursor:"pointer"}, p:3, borderRadius:"50%",opacity:1, transition:"0.3s",   bgcolor:"red", position:"fixed", right:50, bottom:50}}>
+  <Box  sx={{"&:active":{bgcolor:"blue"},  "&:hover":{cursor:"pointer"}, p:2, borderRadius:"50%",opacity:1, transition:"0.3s",   bgcolor:"red", position:"fixed", right:50, bottom:50}}>
     <KeyboardArrowUpIcon sx={{fontSize:"30px", color:"white"}} />
   </Box>
   </a>
