1ef235a8c88d86fecb30b5002fa4be3c296413a0
shehabMm <shehab.mm@gmail.com>
2024-02-06 15:05:40 +0200

    slice

---
+++ ecommerce/frontend/src/Redux/counterSlice.jsx
@@ -0,0 +1,23 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+
+const data ={
+
+count:0
+
+}
+
+const counterSlice = createSlice({
+  name: "productData",
+  initialState: data,
+  reducers: {
+
+increment:()=>{},
+decrement:()=>{}
+
+  }
+  
+
+
+
+}) 
