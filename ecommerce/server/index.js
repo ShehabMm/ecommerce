5d1ef74ade0084495640bb4978c4ef59fdaf2b49
shehabMm <shehab.mm@gmail.com>
2024-02-16 19:48:52 +0200

    server

--- ecommerce/server/index.js
+++ ecommerce/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require("mongoose")
 
 
 
@@ -8,4 +9,14 @@ app.listen(3001, ()=>{
 console.log('server runs perfectly')
 
 
+})
+
+mongoose.connect('mongodb+srv://shehab:1234@newEcommerce.g4yolth.mongodb.net/newEcommerce?retryWrites=true&w=majority').then(()=>{
+
+console.log('connected')
+
+
+}).catch((err)=>{
+console.log(err.message)
+console.log("no")
 })
